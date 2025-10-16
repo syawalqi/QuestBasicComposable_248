eae7659bd89d1a9265bb280c03a713371ac25195
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-16 14:53:28 +0700

    Menambahkan gambar notasibalok

--- app/src/main/java/com/example/myapplication/Tataletak.kt
+++ app/src/main/java/com/example/myapplication/Tataletak.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
