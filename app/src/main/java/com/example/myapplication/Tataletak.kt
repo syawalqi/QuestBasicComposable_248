688f223758447dadc07f1300ac85f2d0af5ffde1
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-16 14:32:47 +0700

    Memebuat fun TataLetakBox

--- app/src/main/java/com/example/myapplication/Tataletak.kt
+++ app/src/main/java/com/example/myapplication/Tataletak.kt
@@ -45,4 +45,21 @@ fun TataletakRow(modifer: Modifier){
     }
 }
 
+@Composable
+fun TataLetakBox(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column 2")
+    }
+}
+
+
 
